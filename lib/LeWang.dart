1414e1fcfadfdc2f7d06a2cbe720e7d8ed21abea
Lewang <1090114998@qq.com>
2019-04-15 23:40:33 +0800

    homepage v0.6:重写主页的 Widget 构造，优化封装。

--- lib/LeWang.dart
+++ lib/LeWang.dart
@@ -7,22 +7,26 @@ const BlockTitleTextStyle = TextStyle(
   color: Colors.white,
 ); // TextStyle
 
-const BlockShadow = BoxShadow(
-    color:Colors.black54,
-    offset: Offset(4.0,4.0),
-    blurRadius: 8.0
-);
+const BlockShadow =
+    BoxShadow(color: Colors.black54, offset: Offset(4.0, 4.0), blurRadius: 8.0);
 
 class DefaultTheme {
   static const buttomColor = Colors.pinkAccent;
 }
+
+//把之前的DecorationBox换成FadeInImage提示图片获取效果
+//方法:因为FadeInImage是Widget而不是decoration所以要用Stack布局
+//先判断图片Url是否为Nil如果为nil的话返回decoration如果不为Nil的话返回FadeInImage
+//PS:把id改成URL 并且对url进行判断
+//网站: https://docs.flutter.io/flutter/widgets/FadeInImage-class.html
+//https://flutterchina.club/cookbook/images/fading-in-images/
 
 class BlockPage extends StatefulWidget {
   @override
   _BlockPageState createState() => new _BlockPageState();
 }
 
-class _BlockPageState extends State<BlockPage> {
+class _BlockPageState extends State<BlockPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     print("BuildingState");
@@ -30,11 +34,13 @@ class _BlockPageState extends State<BlockPage> {
     return new Scaffold(
       body: new StaggeredGridView.count(
           crossAxisCount: 2,
-          staggeredTiles: counter?_staggeredTitles:_staggeredTitles1, //the style of the blocks
-          children: counter?titles:changedTitles, // the information of the blocks
+          staggeredTiles: counter ? _staggeredTitles : _staggeredTitles1,
+          //the style of the blocks
+          children: counter ? titles : changedTitles,
+          // the information of the blocks
           mainAxisSpacing: 12.0,
           crossAxisSpacing: 12.0,
-          padding: EdgeInsets.symmetric(vertical: 78,horizontal: 8)
+          padding: EdgeInsets.symmetric(vertical: 78, horizontal: 8),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
@@ -45,66 +51,120 @@ class _BlockPageState extends State<BlockPage> {
     );
   }
 
-  void onTapFloatButton () {
-    this.setState((){
+  void onTapFloatButton() {
+    setState(() {
       counter = !counter;
     });
   }
 }
 
-class _Blocks extends StatelessWidget {
-  _Blocks(this.id, this.newsTitle, this.bgPic);
+class Blocks extends StatefulWidget {
+  Blocks(this.url, this.newsTitle, this.keyWords);
 
-  var id;
+  var url;
   var newsTitle;
-  var bgPic;
+  List keyWords;
+
+  @override
+  State<StatefulWidget> createState() => BlocksState(url, newsTitle, keyWords);
+}
+
+class BlocksState extends State<Blocks> {
+  BlocksState(this.url, this.newsTitle, this.keyWords);
+
+  var url;
+  var newsTitle;
+  List keyWords;
 
   @override
   Widget build(BuildContext context) {
-    //把之前的DecorationBox换成FadeInImage提示图片获取效果
-    //方法:因为FadeInImage是Widget而不是decoration所以要用Stack布局
-    //先判断图片Url是否为Nil如果为nil的话返回decoration如果不为Nil的话返回FadeInImage
-    //PS:把id改成URL 并且对url进行判断
-    //网站: https://docs.flutter.io/flutter/widgets/FadeInImage-class.html
-    //https://flutterchina.club/cookbook/images/fading-in-images/
+    // TODO: implement build
     return new GestureDetector(
-      child: Container(
-        decoration: PicBoxDecoration(bgPic),
-        child: new Container(
-          decoration: TextBoxDecoration(bgPic),
-          child: new Container(
-            child: Stack(
-              children: <Widget>[
-                Text(
-                  newsTitle,
-                  style: BlockTitleTextStyle,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 5,
-                ),
-                Positioned(
-                  child: Row(
-                    children: BlockKeyWords("keyWord0", "keyWord1"),
-                  ),
-                  bottom: 0.2,
-                  right: 0.2,
-                ),
-              ],
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          BlocksBackgroundPic(url),
+          url == "Nil"
+              ? TitleWithGlass(newsTitle)
+              : TitleWithoutGlass(newsTitle),
+          Positioned(
+            child: Row(
+              children: BlocksKeyWords(keyWords),
             ),
-            padding: EdgeInsets.all(10),
+            bottom: 10.0,
+            right: 10.0,
           ),
-        ),
+        ],
       ),
-      onTap: (){
-        Navigator.push(context,
-            new MaterialPageRoute(builder: (context) {
-              return new BlocksTapRoute(id);
-            }));
-      },
+      onTap: onBlocksTap,
+    );
+  }
+
+  void onBlocksTap(){
+    print(url);
+  }
+}
+
+Widget BlocksBackgroundPic(url) {
+  if (url.toString() == "Nil") {
+    return new Container(
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [BlockShadow],
+        color: Colors.pinkAccent,
+      ),
+    );
+  } else {
+    return new Container(
+      decoration: new BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [BlockShadow],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          url,
+          fit: BoxFit.cover,
+        ),
+//        child: FadeInImage.assetNetwork(
+//          placeholder: "images/test.JPG",
+//          image: url,
+//          fit: BoxFit.cover,
+//        ),
+      ),
     );
   }
 }
 
-List<Widget> BlockKeyWords (keyWord0,keyWord1){
+Widget TitleWithGlass(newsTitle) {
+  return new Container(
+    child: Text(
+      newsTitle,
+      style: BlockTitleTextStyle,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 5,
+    ),
+    padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+  );
+}
+
+Widget TitleWithoutGlass(newsTitle) {
+  return new Container(
+    child: Text(
+      newsTitle,
+      style: BlockTitleTextStyle,
+      overflow: TextOverflow.ellipsis,
+      maxLines: 5,
+    ),
+    padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      color: Color.fromRGBO(14, 14, 14, 0.3),
+    ),
+  );
+}
+
+List<Widget> BlocksKeyWords(keyWords) {
   return <Widget>[
     Container(
       decoration: BoxDecoration(
@@ -112,10 +172,11 @@ List<Widget> BlockKeyWords (keyWord0,keyWord1){
         color: Color.fromRGBO(96, 98, 92, 0.6),
       ),
       height: 21,
-      child:Center(child: Text(keyWord0.toString(),
-          style: TextStyle(
-            color: Colors.white70,
-          )),
+      child: Center(
+        child: Text(keyWords[0].toString(),
+            style: TextStyle(
+              color: Colors.white70,
+            )),
       ),
       padding: EdgeInsets.symmetric(horizontal: 8),
     ),
@@ -128,48 +189,20 @@ List<Widget> BlockKeyWords (keyWord0,keyWord1){
         color: Color.fromRGBO(96, 98, 92, 0.6),
       ),
       height: 21,
-      child:Center(child: Text(keyWord1.toString(),
-          style: TextStyle(
-            color: Colors.white70,
-          )),
+      child: Center(
+        child: Text(keyWords[1].toString(),
+            style: TextStyle(
+              color: Colors.white70,
+            )),
       ),
       padding: EdgeInsets.symmetric(horizontal: 8),
     )
   ];
 }
 
-BoxDecoration PicBoxDecoration(bgPic){
-  if(bgPic == null){
-    return new BoxDecoration(
-      borderRadius: BorderRadius.circular(20),
-      boxShadow: [BlockShadow],
-      color: Colors.pinkAccent, //if the pic is null use the colors
-    );
-  }
-  else{
-    return new BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage(bgPic), //it's an AssetImage from local,can changed with NetworkImage
-        fit: BoxFit.cover,
-      ),
-      borderRadius: BorderRadius.circular(20),
-      boxShadow: [BlockShadow],
-    );
-  }
-}
 
-BoxDecoration TextBoxDecoration(bgPic){
-  if(bgPic == null){
-    return BoxDecoration(); // non pic without color
-  }
-  else{
-    return new BoxDecoration(
-      color: Color.fromRGBO(14, 14, 14, 0.3), //this is the color between the image and text
-      borderRadius: BorderRadius.circular(20),
-    );
-  }
-}
 
+// static fake datas
 List<StaggeredTile> _staggeredTitles = <StaggeredTile>[
   StaggeredTile.count(2, 1),
   StaggeredTile.count(1, 2),
@@ -187,17 +220,17 @@ List<StaggeredTile> _staggeredTitles1 = <StaggeredTile>[
 bool counter = true;
 
 List<Widget> titles = <Widget>[
-  _Blocks(0,titlestext[3],null),
-  _Blocks(1,titlestext[0],"images/bgpic1.png"),
-  _Blocks(2,titlestext[1],"images/bgpic2.jpg"),
-  _Blocks(3,titlestext[2],"images/bgpic3.jpeg"),
+  Blocks("Nil", titlestext[3], keyWords),
+  Blocks(Urls[0], titlestext[0], keyWords),
+  Blocks(Urls[1], titlestext[1], keyWords),
+  Blocks(Urls[2], titlestext[2], keyWords),
 ];
 
 List<Widget> changedTitles = <Widget>[
-  _Blocks(0,titlestext[1],"images/bgpic0.jpg"),
-  _Blocks(1,titlestext[2],"images/bgpic1.png"),
-  _Blocks(2,titlestext[0],"images/bgpic2.jpg"),
-  _Blocks(3,titlestext[3],"images/bgpic3.jpeg"),
+  Blocks(Urls[2], titlestext[1], keyWords),
+  Blocks(Urls[0], titlestext[2], keyWords),
+  Blocks("Nil", titlestext[0], keyWords),
+  Blocks(Urls[1], titlestext[3], keyWords),
 ];
 
 List<String> titlestext = <String>[
@@ -207,28 +240,10 @@ List<String> titlestext = <String>[
   "you build up hope and failures all you know,remember all the sadness and frustration,and let it go,let it go"
 ];
 
-class BlocksTapRoute extends StatefulWidget{
-  BlocksTapRoute(this.id);
+List<String> Urls = <String>[
+  "https://ss0.baidu.com/7Po3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=575b36d70d3b5bb5a1d726fe06d2d523/a6efce1b9d16fdfad03ef192ba8f8c5494ee7b7f.jpg",
+  "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555349644378&di=cd5a913a50105a2c5edb80883c38bf19&imgtype=0&src=http%3A%2F%2Fimg.sgamer.com%2Fwww_sgamer_com%2Fimages%2F190312%2F42ce769a83c98fdd8e8d54ef4591a122.jpg",
+  "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1757326236,2029116386&fm=26&gp=0.jpg"
+];
 
-  var id;
-  @override
-  State<StatefulWidget> createState() => new _BlocksTapRouteState(id);
-
-}
-
-class _BlocksTapRouteState extends State<BlocksTapRoute>{
-  _BlocksTapRouteState(this.id);
-
-  var id;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return new Scaffold(
-      body: Center(
-        child: Text(id.toString()),
-      ),
-    );
-  }
-
-}
+List<String> keyWords = <String>["key1", "key2"];

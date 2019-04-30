4c0b7b90e36db01d8c093de7a358ae8bd35d96a5
FinchFeng <329650437@qq.com>
2019-04-30 20:46:55 +0800

    添加主页返回逻辑代码 注意添加了tuple第三方库

--- lib/MainScreenModel.dart
+++ lib/MainScreenModel.dart
@@ -3,15 +3,55 @@ import 'Network.dart';
 import 'package:flutter/material.dart';
 import 'Constants.dart';
 import 'BlockPage.dart';
+import 'package:tuple/tuple.dart';
 
 class MainModel{
 
+  //添加
   //获取数据颜色
   var currentTilesIndex = 0;
   var currentTilesColorIndex = 0;
+  var currentCategoryArray = ["python","swift"];
+  var currentQueueHeadArray = [1,1];//储存这个值
 
-  var currentCategoryArray = ["java","python","swift"];
-  var currentQueueHeadArray = [1,1,1];
+  //获取每次登陆时间，进行判定是否需要更新queueHead
+
+  //保存近5个页面数据备用 使用元组（第三方）
+  List<Tuple2<List<StaggeredTile>, List<Blocks>>> _oldScreenDatas = [];
+
+  //保存数据有关代码
+  var oldScreenShowingIndex = 0;
+
+  void saveScreenData(tilesArray,blocksArray){
+    if (_oldScreenDatas.length == 4){
+      _oldScreenDatas.removeAt(0);
+    }
+    _oldScreenDatas.add(Tuple2(tilesArray, blocksArray));
+  }
+
+  //获取上一页或者下一页的代码
+  Tuple2<List<StaggeredTile>, List<Blocks>> getUpPageData(){
+    if (this.oldScreenShowingIndex > 0) {
+      this.oldScreenShowingIndex -= 1;
+      return getOldScreenData(oldScreenShowingIndex);
+    }
+  }
+
+  Tuple2<List<StaggeredTile>, List<Blocks>> getDownPageData(){
+    if (this.oldScreenShowingIndex < 4) {
+      this.oldScreenShowingIndex += 1;
+      return getOldScreenData(oldScreenShowingIndex);
+    }
+  }
+
+
+  Tuple2<List<StaggeredTile>, List<Blocks>> getOldScreenData(index){//index只能在1~5
+    return _oldScreenDatas[5-index];
+  }
+
+
+
+
   //获取tiles
   List<StaggeredTile> getATileList(heightIs4){
     var tileArray = heightIs4 ? ConstantsForTile.staggeredTiles4by2 : ConstantsForTile.staggeredTiles3by2;
@@ -60,10 +100,11 @@ class MainModel{
       Blocks widget = Blocks.withJson(Key(id),data,height*Constants.maxline,color);
       result.add(widget);
     });
-
     //更新
     return result;
   }
+
+
 
 
 }
